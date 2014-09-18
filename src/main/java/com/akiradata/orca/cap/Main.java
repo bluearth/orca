e996c8ee0646111434f3843f1de495befc69a0b1
adisuryobaskoro <adisuryobaskoro@gmail.com>
2014-09-18 15:08:52 +0700

    FIRST BLOOD!!!

--- src/main/java/com/akiradata/orca/cap/Main.java
+++ src/main/java/com/akiradata/orca/cap/Main.java
@@ -1,5 +1,7 @@
 package com.akiradata.orca.cap;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,9 +12,24 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		Scene root = FXMLLoader.load(getClass().getResource("root.fxml"));
+		stage.setTitle("ORCA v0.1");
 		stage.setScene(root);		
 		stage.show();
 	}
+	
+	public void onAction() throws IOException {
+       Scene secondScene = FXMLLoader.load(getClass().getResource("CreateNewProject.fxml"));
+
+       Stage secondStage = new Stage();
+       secondStage.setTitle("Create New Project");
+       secondStage.setScene(secondScene);
+        
+       //Set position of second window, related to primary window.
+       secondStage.setX(100 + 250);
+       secondStage.setY(100 + 100);
+
+       secondStage.show();
+    }
 
 	public static void main(String[] args) {
 		launch(args);
